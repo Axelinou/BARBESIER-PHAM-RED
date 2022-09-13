40ecde96adf3992ea176c63b91be54687f8cab6b
cannelle ronron <alexandre.pham@ynov.com>
2022-09-13 17:20:10 +0200

    index.go

--- index.go
+++ index.go
@@ -1,35 +1,45 @@
-package main 
+package main
+
 import "fmt"
 
 type Personnage struct {
-	Nom string
-	Classe string
-	Niveau int
-	Pvmax int
-	Pv int 
-	inventaire []string
-
+	Nom         string
+	Classe      string
+	Niveau      int
+	Pvmax       int
+	Pv          int
+	inp1entaire []string
 }
+
 func main() {
-	init(perso1)
+	var p1 Personnage
+	p1.Nom = "Suad"
+	p1.Classe = "Chômeur"
+	p1.Niveau = 3
+	p1.Pvmax = 10
+	p1.Pv = 1
+	//fmt.Println(p1)
+	p1.display()
+	//p1.setPower()
+	//p1.LessPower()
 }
 
-func init(perso1) {
-	var perso1
-	perso1.nom = "éclairus"
-	perso1.classe =  "warrior"
-	perso1.niveau = 1
-	perso1.pvmax = 150
-	perso1.pv = 100
-	perso1.inventaire = []string{"mace","shield","armor,healthpotion"}
+func (p1 Personnage) display() {
+	fmt.Println("----------------")
+	fmt.Println("Nom du joueur:", p1.Nom)
+	fmt.Println("Classe:", p1.Classe)
+	fmt.Println("Santé:", p1.Pv)
+	fmt.Println("Niveau:", p1.Niveau)
+	fmt.Println("Santé Max :", p1.Pvmax)
+	fmt.Println("----------------")
 
 }
+func (p1 *Personnage) setPower() {
+	p1.Niveau += 100
+	fmt.Println("Power:", p1.Niveau)
+}
 
-func (Personnage) display() {
-	fmt.Println("Nom:",perso01.nom)
-	fmt.Println("Classe:",perso01.classe)
-	fmt.Println("Niveau:",perso01.niveau)
-	fmt.Println("Pvmax:",perso01.pvmax)
-	fmt.Println("Pv:",perso01.pv)
-	fmt.Println("Inventaire:",perso01.inventaire)
+func (p1 *Personnage) LessPower() {
+	p1.Niveau -= 100
+	fmt.Println("Power:", p1.Niveau)
 }
